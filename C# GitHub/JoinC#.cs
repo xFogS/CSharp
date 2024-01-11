7bc1951e20eb2f893f6de205bb3c07d2272ee168
xFogS <131852082+xFogS@users.noreply.github.com>
2024-01-11 19:59:23 +0200

    JoinC#.cs

--- C# GitHub/JoinC#.cs
+++ C# GitHub/JoinC#.cs
@@ -1,11 +1,4 @@
-﻿//***Користувач вводить з клавіатури число від 1 до 100. 
-//Якщо число кратне 3 (ділиться на 3 без залишку), потрібно 
-//вивести слово Fizz. Якщо число кратне 5, потрібно вивести 
-//слово Buzz. Якщо число кратне 3 і 5, потрібно вивести Fizz
-//Buzz. Якщо число не кратне ні 3, ані 5, потрібно вивести 
-//тільки число.
-//Якщо користувач ввів значення не в діапазоні від 1
-//до 100, потрібно вивести повідомлення про помилку.
+//1
 
 int inputUserNum = Convert.ToInt32(Console.ReadLine());
 if (inputUserNum >= 1 && inputUserNum <= 100)
@@ -17,18 +10,13 @@ if (inputUserNum >= 1 && inputUserNum <= 100)
 }
 else Console.WriteLine("Range error!");
 
-//***Користувач вводить з клавіатури два числа. Перше 
-//число — це значення, друге число — відсоток, який необхідно підрахувати. Наприклад, ми ввели з клавіатури 
-//90 і 10. Потрібно вивести на екран 10 відсотків від 90. 
-//Результат: 9
+//2
 
 Console.Write("Enter a numeric value: "); int value = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter the value in interest: "); int interest = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"Result: {value / interest}");
 
-//***Користувач вводить з клавіатури чотири цифри. Необхідно створити число, яке містить ці цифри.
-//Наприрклад, якщо з клавіатури введено 1, 5, 7, 8 тоді потрібно 
-//сформувати число 1578
+//3
 
 int fNum, sNum, tNum, foNum;
 Console.Write("Enter the first value: "); fNum = Convert.ToInt32(Console.ReadLine());
@@ -37,12 +25,7 @@ Console.Write("Enter the three value: "); tNum = Convert.ToInt32(Console.ReadLin
 Console.Write("Enter the four value: "); foNum = Convert.ToInt32(Console.ReadLine());
 Console.Write($"Result: {fNum * 1000 + sNum * 100 + tNum * 10 + foNum}");
 
-//***Користувач вводить шестизначне число. Потім, користувач вводить номери розрядів для заміни цифр. 
-//Наприклад, якщо користувач ввів 1 і 6 — це означає, що 
-//треба поміняти місцями першу та шосту цифри. 
-//Число 723895 має перетворитися на 523897.
-//Якщо користувач ввів не шестизначне число, потрібно 
-//вивести повідомлення про помилку.
+//4
 
 Console.Write("Enter the value number: "); int sixValue = Convert.ToInt32(Console.ReadLine());
 int m = 0;
@@ -59,10 +42,7 @@ if (sixValue >= 111111 && sixValue <= 999999)
 else
     Console.WriteLine("Input Error number");
 
-//***Користувач вводить з клавіатури дату. Додаток має 
-//відобразити назву пори року і дня тижня. Наприклад,
-//якщо введено 22.12.2021, додаток має відобразити Winter 
-//Wednesday.
+//5
 
 
 Console.Write("Date: "); DateTime date = Convert.ToDateTime(Console.ReadLine());
@@ -77,9 +57,7 @@ switch (date.Month)
         Console.WriteLine("Month from 1 to 12"); break;
 }
 
-//Користувач вводить з клавіатури показання температури. Залежно від вибору користувача, додаток конвертує 
-//температуру з Фаренгейта в Цельсій, або навпаки. 
-
+//6
 Console.WriteLine("1. with Fahrenheit in Celsius\n2. with Celsius in Fahrenheit");
 int _userInp = Convert.ToInt32(Console.ReadLine());
 
@@ -104,11 +82,7 @@ switch (_userInp)
         Console.WriteLine("Input error!"); break;
 }
 
-//Користувач вводить з клавіатури два числа. Потрібно показати усі парні числа у вказаному діапазоні. Якщо 
-//межі діапазону вказані неправильно, потрібно провести 
-//нормалізацію кордонів. Наприклад, користувач ввів 20 і 
-//11. Потрібна нормалізація, після якої початок діапазону 
-//дорівнюватиме 11, а кінець дорівнюватиме 20.
+//7
 
 int startRange, endRange;
 Console.Write("Start pos: "); startRange = Convert.ToInt32(Console.ReadLine());
