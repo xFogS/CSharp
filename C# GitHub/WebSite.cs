e1798d051eeb3546008a840ee711b838bedb299d
xFogS <131852082+xFogS@users.noreply.github.com>
2024-01-12 16:22:28 +0200

    Add files via upload

---
+++ C# GitHub/WebSite.cs
@@ -0,0 +1,72 @@
+﻿/*Створіть клас «Веб-сайт».*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace web
+{
+    public class WebSite
+    {
+
+        private string nameSite = "IT UA";
+        private string wayToSite = "https://it.com";
+        private string siteDescription = "Just a Site";
+        private string ipSite = "121.0.0.1";
+
+        public WebSite() { }
+        public WebSite(string nameSite, string wayToSite, string siteDescription, string ipSite)
+        {
+            this.nameSite = nameSite; this.wayToSite = wayToSite;
+            this.siteDescription = siteDescription; this.ipSite = ipSite;
+        }
+        public string NameSite 
+        {   
+            get { return nameSite; }
+            set
+            {
+                Console.Write("New Site: "); string? input = Console.ReadLine();
+                if (input != null && input.Length < 30) nameSite = input;
+                else nameSite = "None";
+            }
+        }
+        public string WayToSite 
+        { 
+            get { return wayToSite; }
+            set
+            {
+                Console.Write("Way to..: "); string? input = Console.ReadLine();
+                if (input != null && input.Length < 100) wayToSite = input;
+                else wayToSite = "None";
+            }
+        }
+        public string SiteDescription 
+        { 
+            get { return siteDescription; }
+            set
+            {
+                Console.Write("Description: "); string? input = Console.ReadLine();
+                if (input != null && input.Length < 150) siteDescription = input;
+                else siteDescription = "None";
+            }
+        }
+        public string IPSite 
+        { 
+            get { return ipSite; }
+            set
+            {
+                Console.Write("IP Adress: "); string? input = Console.ReadLine();
+                if (input != null && input.Length < 150) ipSite = input;
+                else ipSite = "121.0.0.1";
+            }
+        }
+        public void PrintInfo()
+        {
+            Console.Clear();
+            Console.WriteLine($"Name: {nameSite}\nWay To Site: {wayToSite}\n" +
+                $"Description: {siteDescription}\nIP: {ipSite}");
+        }
+    }
+}
---
+++ C# GitHub/lesson3.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Text;
+using web;
+using matrix;
+using static Passport;
+Console.InputEncoding = Encoding.Unicode;
+Console.OutputEncoding = Encoding.Unicode;
+
+/*1*/
+
+void symbolsSqrt(int line, char sym)
+{
+    for (int i = 0; i < line; i++)
+    {
+        for (int j = 0; j < line; j++) Console.Write(sym.ToString() + ' ');
+        Console.WriteLine();
+    }
+}
+
+symbolsSqrt(9, '*');
+
+/*2*/
+
+bool palindrone(int value)
+{
+    int size = value.ToString().Length;
+    string val = value.ToString();
+    for (int i = 0; i < size; i++)
+    {
+        if (val[i] != val[size - 1 - i])
+            return false;
+    }
+    return true;
+}
+
+bool temp = palindrone(345543);
+Console.WriteLine($"Palindrone: {temp}");
+
+
+/*3 */
+
+int[] filtArray(int[] originalArray, int[] filterArray)
+{
+    Array.Sort(originalArray); Array.Sort(filterArray);
+    int[] newArray = new int[originalArray.Length];
+    
+    for (int i = 0; i < originalArray.Length; i++)
+    {
+        for (int j = 0; j < filterArray.Length; j++)
+        {
+        }
+    }
+
+    return newArray;
+}
+
+int[] orgArr = new int[] { 3, 5, 2, 3, 7, 1, 8, 1 };
+int[] filArr = new int[] { 3, 5, 1 };
+int[] newArray = filtArray(orgArr, filArr);
+foreach (var item in newArray)
+{
+    Console.Write($"{item} ");
+}
+
+//4
+
+WebSite site = new WebSite();
+site.NameSite = new WebSite().NameSite;
+site.WayToSite = new WebSite().WayToSite;
+site.SiteDescription = new WebSite().SiteDescription;
+site.IPSite = new WebSite().IPSite;
+site.PrintInfo();
