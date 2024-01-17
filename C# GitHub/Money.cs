95a5d41af6dcf889438cb893b0255ce3d5424160
xFogS <131852082+xFogS@users.noreply.github.com>
2024-01-17 14:58:21 +0200

    Add files via upload

---
+++ C# GitHub/Money.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace less
+{
+    /*Product - просят лише по працювати з цілим числом,центи не зачеплювати як я зрозумів.
+    Щодо доп полів що ви казали, я б на свою думку зробив його одним double*/
+
+    public class Money
+    {
+        protected int money = 0;
+        protected double cent = 0;
+        
+        protected Money() { }
+        public Money(int money, double cent) 
+        {
+            this.money = money;
+            this.cent = cent;
+        }
+
+        public void PrintMoney()
+        {
+            Console.WriteLine($"{money}.{cent}");
+        }
+        public void SetNewSumm()
+        {
+            Console.Write("Sum: "); money = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Cent: "); cent = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+
+    public class Product : Money
+    {
+        private int reduceTheSum = 0;
+        private int summ = 0;
+        public Product() { }
+        public Product(int money, double cent, int reduceTheSum) : base(money, cent)
+        {
+            this.reduceTheSum = reduceTheSum;
+        }
+        public void printProductSum()
+        {
+            base.PrintMoney();
+            if (summ >= 0) Console.WriteLine($"New sum: {summ}.{cent}");
+        }
+        public new void SetNewSumm()
+        {
+            base.SetNewSumm();
+            Console.Write("reduce The Sum: "); reduceTheSum = Convert.ToInt32(Console.ReadLine());
+            if (reduceTheSum >= 0 && reduceTheSum <= money) summ = money - reduceTheSum;
+        }
+    }
+}
---
+++ C# GitHub/less5.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Text;
+using less;
+
+Console.InputEncoding = Encoding.Unicode;
+Console.OutputEncoding = Encoding.Unicode;
+
+Product p = new Product();
+p.SetNewSumm();
+p.printProductSum();
+
+
