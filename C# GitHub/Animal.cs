7e5ef39be186f186c8703154752b1acc8aab5aa3
xFogS <131852082+xFogS@users.noreply.github.com>
2024-01-21 22:33:20 +0200

    Add files via upload

---
+++ C# GitHub/Animal.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        public virtual void Name() { Console.WriteLine("No Name"); }
+        public virtual void Voice() { Console.WriteLine("~~~~~~"); }
+    }
+
+    public class OnTheEarth : Animal {}
+    public sealed class Cat : OnTheEarth
+    {
+        public override void Name() { Console.WriteLine("Cat"); }
+        public override void Voice() { Console.WriteLine("Mau mau"); }
+    }
+    public sealed class Dog : OnTheEarth 
+    {
+        public override void Name() { Console.WriteLine("Dog"); }
+        public override void Voice() { Console.WriteLine("Gaw Gaw"); }
+    }
+
+    public sealed class Hamster : OnTheEarth 
+    {
+        public override void Name() { Console.WriteLine("Hamster"); }
+        public override void Voice() { base.Voice(); }
+    }
+
+    public sealed class HermitСrab : OnTheEarth
+    {
+        public override void Name() { Console.WriteLine("Hermit Сrab"); }
+        public override void Voice() { base.Voice(); }
+    }
+
+    public class Bird : Animal {}
+    public sealed class Eagle : Bird
+    {
+        public override void Name() { Console.WriteLine("Eagle"); }
+        public override void Voice() { Console.WriteLine("ia ia ia ia"); }
+    }
+
+    public sealed class Parrot : Bird 
+    {
+        public override void Name() { Console.WriteLine("Parrot"); }
+        public override void Voice() { Console.WriteLine("kaka ka kaka ka"); }
+    }
+    public sealed class Magpie : Bird
+    {
+        public override void Name() { Console.WriteLine("Magpie"); }
+        public override void Voice() { Console.WriteLine("kakaka"); }
+    }
+    public sealed class Swallow : Bird
+    {
+        public override void Name() { Console.WriteLine("Swallow"); }
+        public override void Voice() { Console.WriteLine("cwi cwi cwi"); }
+    }
+    public class Fish : Animal {}
+    public sealed class Crab : Fish 
+    {
+        public override void Name() { Console.WriteLine("Crab"); }
+        public override void Voice() { base.Voice(); }
+    }
+    public sealed class Jellyfish : Fish
+    {
+        public override void Name() { Console.WriteLine("Jellyfish"); }
+        public override void Voice() { base.Voice(); }
+    }
+    public sealed class Oysters : Fish
+    {
+        public override void Name() { Console.WriteLine("Oysters"); }
+        public override void Voice() { base.Voice(); }
+    }
+    public sealed class Shark : Fish
+    {
+        public override void Name() { Console.WriteLine("Shark"); }
+        public override void Voice() { base.Voice(); }
+    }
+}
---
+++ C# GitHub/Program.cs
@@ -0,0 +1,35 @@
+﻿using Animals;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        OnTheEarth[] earth =
+        {
+            new Cat(),
+            new Dog(),
+            new Hamster(),
+            new HermitСrab()
+        };
+
+        Bird[] birds =
+        {
+            new Eagle(),
+            new Parrot(),
+            new Magpie(),
+            new Swallow(),
+        };
+
+        Fish[] fish =
+        {
+            new Crab(),
+            new Jellyfish(),
+            new Oysters(),
+            new Shark(),
+        };
+
+        foreach (var item in earth) { item.Voice(); }
+        foreach (var item in birds) { item.Voice(); }
+        foreach (var item in fish) { item.Voice(); }
+    }
+}
