8c8ef09e238f8e763f96e3f7e0dc88d62a1e0771
xFogS <131852082+xFogS@users.noreply.github.com>
2024-02-15 00:55:28 +0200

    CreditCard.cs

--- C# GitHub/CreditCard.cs
+++ C# GitHub/CreditCard.cs
@@ -11,28 +11,30 @@ namespace bank
         private string NumberCard { get; }
         private string NamePerson { get; set; }
         private string PINCode { get; set; } //error about set.lenght(4 - default PinCode) ?: - going 
-        private int CreditLimit { get; set; } = 0;
-        private int Balance { get; set; } = 0;
+        private uint CreditLimit { get; set; } = 0;
+        private uint Balance { get; set; } = 0;
 
-        public CreditCard(string number, string person, string pin, int limit)
+        public CreditCard(string number, string person, string pin, uint limit)
         {
             NumberCard = number; NamePerson = person;
             _ = pin.Length == 4 ? PINCode = pin : PINCode = "1111";
             CreditLimit = limit;
         }
-        public void AddSummOnBalance(int summ)
+        public void AddSummOnBalance(uint summ)
         {
             Balance += summ;
             string msg = $"{DateTime.Now} ~ Summ [{summ}] / new Balance [{Balance}]";
-            MainPushMessage?.Invoke($"{msg}");
+            MainPushMessagePhone?.Invoke($"{msg}");
+            MainPushMessageEmail?.Invoke($"{msg}");
         }
-        public void UsingSummOnBalance(int summ)
+        public void UsingSummOnBalance(uint summ)
         {
             if (Balance >= summ)
             {
                 Balance -= summ;
                 string msg = $"You take money. Now your Balance: [{Balance}]";
-                MainPushMessage?.Invoke(msg);
+                MainPushMessagePhone?.Invoke(msg);
+                MainPushMessageEmail?.Invoke(msg);
             }
             else if (CreditLimit >= summ && CreditLimit >= Balance)
             {
@@ -42,14 +44,16 @@ namespace bank
             }
             else if (summ > Balance && summ > CreditLimit)
             {
-                int tempBalance = Balance + CreditLimit;
+                uint tempBalance = Balance + CreditLimit;
                 if (tempBalance >= summ)
                 {
                     tempBalance -= summ;
                     Balance = 0; CreditLimit = 0;
-                    MainPushMessage?.Invoke($"You take money. Now your Balance: [{Balance}]");
+                    MainPushMessagePhone?.Invoke($"You take money. Now your Balance: [{Balance}]");
+                    MainPushMessageEmail?.Invoke($"You take money. Now your Balance: [{Balance}]");
                 }
-                else MainPushMessage?.Invoke("You cannot withdraw your money.You don't have them!");
+                else { MainPushMessagePhone?.Invoke("You cannot withdraw your money.You don't have them!");
+                       MainPushMessageEmail?.Invoke("You cannot withdraw your money.You don't have them!"); }
             }
         }
         public void NewChangePINCode(string pin)
@@ -58,24 +62,26 @@ namespace bank
             {
                 PINCode = pin;
                 string msg = $"\tYour set new PinCode: [{PINCode}]\n\tPlease don't say nobody";
-                MainPushMessage?.Invoke(msg);
+                MainPushMessagePhone?.Invoke(msg);
+                MainPushMessageEmail?.Invoke(msg);
             }
         }
-        public void NewCreditLimit(int limit = 1500)
+        public void NewCreditLimit(uint limit = 1500)
         {
             if (CreditLimit == 0 && limit >= 1500)
             {
                 CreditLimit += limit;
                 Console.WriteLine($"New Credit Limite: {CreditLimit}");
             }
-            else Console.WriteLine("Private Person has Credit. It should be closed");
+            else Console.WriteLine("Private Person has the Credit. It should be closed");
         }
         public void ClientInfo()
         {
             Console.WriteLine($"Number: {NumberCard}\nPerson: {NamePerson}\nPIN: {PINCode}\nLimit: {CreditLimit}\nBalance: {Balance}");
         }
 
-        public event Action<string> MainPushMessage;
+        public event Action<string> MainPushMessagePhone;
+        public event Action<string> MainPushMessageEmail;
         public event Action<string> StartUsingCreditMoney;
     }
 }
@@ -85,14 +91,14 @@ public class Program
     static void Main(string[] args)
     {
         CreditCard card = new CreditCard("9342 5325 2394 3249", "FDS SDF ASD", "43823", 1500);
-        card.MainPushMessage += Card_PushOnPhone;
-        card.MainPushMessage += Card_PushOnEmail;
+        card.MainPushMessagePhone += Card_PushOnPhone;
+        card.MainPushMessageEmail += Card_PushOnEmail;
         card.StartUsingCreditMoney += Card_StartUsingCreditMoney;
 
         card.ClientInfo();
         card.AddSummOnBalance(200);
         card.AddSummOnBalance(30);
-        card.UsingSummOnBalance(500);
+        card.UsingSummOnBalance(300);
         card.AddSummOnBalance(500);
         card.UsingSummOnBalance(200);
         card.NewChangePINCode("9321");
@@ -100,7 +106,6 @@ public class Program
         card.UsingSummOnBalance(1000);
         card.ClientInfo();
     }
-
 
     private static void Card_PushOnPhone(string obj) => Console.WriteLine($"Phone: {obj}");
     private static void Card_PushOnEmail(string obj) => Console.WriteLine($"Email: {obj}");
